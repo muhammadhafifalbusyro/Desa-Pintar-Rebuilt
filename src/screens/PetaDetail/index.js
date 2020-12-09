ea4b6b84dbe200ececa0a136e8dd707764e4301e
Muhammad Hafif Al Busyro <muhammadhafifalbusyro@gmail.com>
2020-12-09 11:04:27 +0700

    peta detail

--- src/screens/PetaDetail/index.js
+++ src/screens/PetaDetail/index.js
@@ -8,51 +8,37 @@ import {
   TouchableOpacity,
   TouchableNativeFeedback,
   TextInput,
+  ImageBackground,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 class PetaDetail extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Icon
-            name="arrow-left"
-            size={30}
-            color="white"
-            onPress={() => this.props.navigation.navigate('Lapor')}
-          />
-          <Text style={styles.textHeader}>Peta</Text>
-        </View>
-        <ScrollView style={styles.scroll}>
-          <View style={styles.cameraContainer}>
-            <Image
-              style={styles.boxCamera}
-              source={{
-                uri:
-                  'https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/10/5-Characters-Sanji-Can-Beat.jpg',
-              }}
-            />
-          </View>
+  state = {
+    promosi: false,
+  };
+  promosi = () => {
+    if (this.state.promosi) {
+      return (
+        <View style={{width: '100%'}}>
           <View style={styles.boxContent}>
-            <Text style={styles.text1}>Pilih Kategori</Text>
+            <Text style={styles.text1}>Jual/Sewa/Kerja sama</Text>
             <View style={styles.childBox}>
-              <Text>Pertanian</Text>
+              <Text>Jual</Text>
               <Icon name="chevron-down" size={25} />
             </View>
           </View>
           <View style={styles.boxContent}>
-            <Text style={styles.text1}>Judul</Text>
-            <TextInput style={styles.childBox} placeholder="Masukan Judul" />
+            <Text style={styles.text1}>Harga Per M2</Text>
+            <TextInput style={styles.childBox} placeholder="Masukan Harga" />
           </View>
           <View style={styles.boxContent}>
-            <Text style={styles.text1}>Deskripsi</Text>
+            <Text style={styles.text1}>Narasi</Text>
             <TextInput
               style={{
                 ...styles.childBox,
                 height: 150,
               }}
-              placeholder="Masukan Deskripsi"
+              placeholder="Masukan Narasi"
               textAlignVertical="top"
             />
           </View>
@@ -68,10 +54,74 @@ class PetaDetail extends React.Component {
                 }}
                 placeholder="Masukan Deskripsi"
                 textAlignVertical="top">
-                <Text style={{color: 'white'}}>LAPORKAN</Text>
+                <Text style={{color: 'white'}}>PUBLISH</Text>
               </View>
             </TouchableNativeFeedback>
           </View>
+        </View>
+      );
+    }
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Icon
+            name="arrow-left"
+            size={30}
+            color="white"
+            onPress={() => this.props.navigation.navigate('Peta')}
+          />
+          <Text style={styles.textHeader}>Peta</Text>
+        </View>
+        <ScrollView style={styles.scroll}>
+          <View style={styles.cameraContainer}>
+            <ImageBackground
+              style={styles.boxCamera}
+              source={{
+                uri:
+                  'https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/10/5-Characters-Sanji-Can-Beat.jpg',
+              }}>
+              <TouchableNativeFeedback>
+                <View style={styles.boxLiihat}>
+                  <Text style={styles.textLihat}>Lihat Peta</Text>
+                </View>
+              </TouchableNativeFeedback>
+            </ImageBackground>
+          </View>
+          <Text style={{marginHorizontal: 10, marginBottom: 5}}>
+            Luas : 2500m2
+          </Text>
+          <Text style={{marginHorizontal: 10, marginBottom: 5}}>
+            Status Hak : 2500m2
+          </Text>
+          <Text style={{marginHorizontal: 10, marginBottom: 5}}>
+            Penggunaan Tanah : 2500m2
+          </Text>
+          <Text style={{marginHorizontal: 10, marginBottom: 5}}>
+            Pemanfaatan Tanah : 2500m2
+          </Text>
+          <Text style={{marginHorizontal: 10, marginBottom: 10}}>
+            RT/RW : 2500m2
+          </Text>
+          <View style={styles.boxContent}>
+            <TouchableNativeFeedback
+              onPress={() => this.setState({promosi: !this.state.promosi})}>
+              <View
+                style={{
+                  ...styles.childBox,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: '#ffda77',
+                  borderColor: 'white',
+                }}
+                placeholder="Masukan Deskripsi"
+                textAlignVertical="top">
+                <Text>PROMOSI</Text>
+              </View>
+            </TouchableNativeFeedback>
+          </View>
+          {this.promosi()}
         </ScrollView>
       </View>
     );
@@ -113,8 +163,8 @@ const styles = StyleSheet.create({
     width: '100%',
     borderRadius: 5,
     borderColor: 'grey',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
   },
   boxContent: {
     paddingHorizontal: 10,
@@ -135,5 +185,14 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  boxLiihat: {
+    height: 30,
+    width: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffda77',
+    margin: 10,
+    borderRadius: 5,
   },
 });
